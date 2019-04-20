80e62d6814523a6e338c7fef02a0353d2693721e
Bruno Ferreira Coelho <bruno.f.coelho@ufv.br>
2019-04-20 14:37:36 -0300

    Parte 2 Pronta: Preditor de 2 bits implementado

--- TP1/branch_table.v
+++ TP1/branch_table.v
@@ -23,7 +23,7 @@ module branch_table(
 	parameter tagData = "tag_data.txt";
 	parameter pData = "p_data.txt";
 	reg[25:0] tag[0:15];
-	reg pred[0:15];
+	reg[1:0] pred[0:15];
 	reg[31:0] dest [0:15];
 	wire[3:0] prefix;
 	wire[25:0] sufix;
@@ -41,13 +41,37 @@ module branch_table(
 			dest[sufix] <= b_dest_in;
 		end
 		if(write_rp) begin
-			pred[sufix] <= deviated_s4;
+			// pred[sufix] <= deviated_s4;
+			if(deviated_s4==1'b1 & pred[sufix]==2'd0) begin
+				pred[sufix] <= 2'd1;
+			end
+			else if(deviated_s4==1'b1 & pred[sufix]==2'd1) begin
+				pred[sufix] <= 2'd2;
+			end
+			else if(deviated_s4==1'b1 & pred[sufix]==2'd2) begin
+				pred[sufix] <= 2'd3;
+			end
+			else if(deviated_s4==1'b1 & pred[sufix]==2'd3) begin
+				pred[sufix] <= 2'd3;
+			end
+			else if(deviated_s4==1'b0 & pred[sufix]==2'd3) begin
+				pred[sufix] <= 2'd2;
+			end
+			else if(deviated_s4==1'b0 & pred[sufix]==2'd2) begin
+				pred[sufix] <= 2'd1;
+			end
+			else if(deviated_s4==1'b0 & pred[sufix]==2'd1) begin
+				pred[sufix] <= 2'd0;
+			end
+			else if(deviated_s4==1'b0 & pred[sufix]==2'd0) begin
+				pred[sufix] <= 2'd0;
+			end     
 		end
 	end
 
 	assign b_dest_out = dest[inst_adress_s1[5:2]][31:0];
 	assign hit_s1 = (tag[inst_adress_s1[5:2]] == inst_adress_s1[31:6]);
-	assign p_s1 = pred[inst_adress_s1[5:2]];
+	assign p_s1 = pred[inst_adress_s1[5:2]][1];
 endmodule
 
 `endif
